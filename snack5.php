2cb61690b507ad537e366425f47eb841c5597bf5
Deceit-87 <manuelfreund318@gmail.com>
2022-03-29 22:03:42 +0200

    snack 5 end

---
+++ snack5.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack 5</title>
+</head>
+<body>
+    
+<?php 
+
+
+$paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quisquam.
+Deserunt cumque eum libero temporibus corrupti fugiat unde molestiae sapiente.
+Unde cumque autem laborum, nisi quod repellendus omnis! Incidunt, laudantium.
+Quia ducimus odio nobis ut delectus iure possimus magnam ullam!
+Laudantium doloremque, voluptatibus eaque rerum facere nobis! Libero, architecto itaque.';
+
+$paragrafo_diviso = explode('.',$paragrafo);
+var_dump($paragrafo_diviso);
+
+
+?>
+
+</body>
+</html>
